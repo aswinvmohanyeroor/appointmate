4d5af51dc3f205e38ec53891ea9ea96646d67d54
aswinvmohanyeroor <aswinvmohanyeroor@gmail.com>
2024-03-15 00:50:44 +0530

    feat(client): minor fixes

--- src/utils/auth-config.js
+++ src/utils/auth-config.js
@@ -1,18 +1,12 @@
+/* eslint-disable no-undef */
 import { LogLevel } from "@azure/msal-browser"
 
-// auth: {
-//     clientId: "04c07748-9487-4ab3-804f-a2a5172db6f3", // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
-//     authority: "https://login.microsoftonline.com/" + "f0132019-19b9-4aad-a382-6c04f1b4f18b", // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
-//     clientSecret: "EXb8Q~bl.B2Kmat6tlwYgLo862TE8NVerj2kkdux", // Client secret generated from the app registration in Azure portal
-//     redirectUri: "http://localhost:3000/appointments", // This is the redirect URI of the app registration in Azure portal
-//     postLogoutRedirectUri: "http://localhost:3000/",
-//     navigateToLoginRequestUrl: false
-// },
+
 export const msalConfig = {
     auth: {
-        clientId: "ce4aeb81-d2c8-480e-967e-f69dffde55c3", // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+        clientId: process.env.MSAL_CLIENT, // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
         authority: "https://login.microsoftonline.com/common",// Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
-        clientSecret: "1xK8Q~CA6xe.2sRhhQ.I~LZcUoOp26_2QjNyga71", // Client secret generated from the app registration in Azure portal
+        clientSecret: process.env.MSAL_SECRET, // Client secret generated from the app registration in Azure portal
         redirectUri: "https://appointmate-three.vercel.app/appointments", // This is the redirect URI of the app registration in Azure portal
         postLogoutRedirectUri: "https://appointmate-three.vercel.app/",
         navigateToLoginRequestUrl: false
